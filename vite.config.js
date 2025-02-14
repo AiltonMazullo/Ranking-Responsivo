ec433e06ac964f7b3a0bfdd292973eaff9b07d3f
Your Name <you@example.com>
2025-02-14 13:10:22 -0300

    configurações de deploy

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Ranking-Responsivo/', // Caminho correto para GitHub Pages
+  base: '/Ranking-Responsivo/', // Caminho correto para GitHub Pages (Nome do respositório)
   resolve: {
     extensions: ['.js', '.jsx'], // Garante que o Vite reconheça arquivos .jsx
   },
